15890e9d69feb3dd57f340738ba12c882b166a99
Vinay Kaushik <kumar.vinay@gmail.com>
2025-01-04 20:51:21 +0530

    prodcut service swagger configured

--- product-service/src/main/java/com/ps/productservice/config/KafkaConfig.java
+++ product-service/src/main/java/com/ps/productservice/config/KafkaConfig.java
@@ -5,9 +5,9 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
@@ -16,11 +16,11 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@EnableKafka
 @Configuration
 public class KafkaConfig {
 
-    private final String bootstrapServers = "localhost:9092";
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
--- user-management-service/src/main/java/com/ps/userservice/config/SwaggerConfig.java
+++ user-management-service/src/main/java/com/ps/userservice/config/SwaggerConfig.java
@@ -1,3 +1,5 @@
+package com.ps.userservice.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
